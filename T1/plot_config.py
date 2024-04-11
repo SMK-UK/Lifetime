04a0318eae046424c297246e3189a5fbee0ee7a0
SMK-UK <keenan.sean@hotmail.co.uk>
2024-04-11 23:50:02 +0100

    general update

--- T1/plot_config.py
+++ T1/plot_config.py
@@ -6,7 +6,7 @@ config_params = dict(
     
     # Path to load data from and arguments used to disriminate files loaded
     
-    root                = r"C:\Users\keena\Documents\University\PhD\Code",
+    root                = r"C:\Users\keena\Documents\University\python_scripts\Lifetime\T1",
     file                = r"0329_Fluor_duration_fit.json"
 
 )
